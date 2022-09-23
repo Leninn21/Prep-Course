aaf2bc7fdb6ab0bfdbb1a4b85a271b96e3f45887
Leninn <leninnvillegasdiaz@gmail.com>
2022-09-22 21:17:47 -0500

    corrección de practica

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,7 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  retun str;
+  return str;
   
 }
 
@@ -36,26 +36,26 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  retun x + y ;
+  return x + y ;
   
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  retun x - y ;
+  return x - y ;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  retun x * y ;
+  return x * y ;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  retun x / y ;
+  return x / y ;
 }
 
 function sonIguales(x, y) {
@@ -63,9 +63,9 @@ function sonIguales(x, y) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (x === y) {
-     reun  true;
+     return  true;
      } else {
-      reun  false;
+      return  false;
      }
     }
 
@@ -73,14 +73,14 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  retun str1.length === str2.length;
+  return str1.length === str2.length;
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num < 90){retun true ; else } {retun false; }
+  if (num < 90) {return true ;} else {return false; }
   
 }
 
@@ -88,20 +88,20 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  reun num > 50;
+  return num > 50;
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  retun x % y;
+  return x % y;
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 === 0) {retun true;} else {retun false;}
+  if (num % 2 === 0) {return true;} else {return false;}
   
 }
 
@@ -109,7 +109,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 === 1) {retun true;} else {retun false} 
+  if (num % 2 === 1) {return true;} else {return false} 
   
 }
 
@@ -117,42 +117,42 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  retun match.pow(num, 2);
+  return Math.pow(num, 2);
   
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  retun num * num * num;
+  return num * num * num;
   
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  retun match.pow(num, exponent);
+  return Math.pow(num, exponent);
   
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  reun match.round(num);
+  return Math.round(num);
   
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  retun match.ceil(num);
+  return Math.ceil(num);
   
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  retun match.random();
+  return Math.random();
 }
 
 function esPositivo(numero) {
@@ -160,22 +160,22 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if(nuemro === 0){retun false ;} else if (numero > 0 )
-{retun "es positivo" ;} else {retun " es negativo";}  
+ if(numero === 0){return false;} else if(numero > 0)
+{return "Es positivo";} else {return "Es negativo";} 
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  retun str +"!";
+  return str +"!";
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinado = nombre+ " " + apellido; retun combinado;
+  var combinado = nombre+ " " + apellido; return combinado;
   
 }
 
@@ -183,14 +183,14 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  retun "hola" + nombre  + "!";
+  return "Hola " + nombre  + "!";
   
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  retun alto * ancho;
+  return alto * ancho;
   
 }
 
@@ -198,7 +198,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  retun lado *4
+  return lado *4
   
 }
 
@@ -206,7 +206,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-  retun (base  * altura) /2 
+  return (base  * altura) /2 
 
 }
 
@@ -215,7 +215,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  retun euro * 1.2
+  return euro * 1.2
   
 }
 
@@ -226,9 +226,9 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aqui
-  if (letra.length > 1) {retun "dato incorrecto"}
+  if (letra.length > 1) {return "Dato incorrecto"}
 if (letra ==="a" || letra  === "e" || letra === "i" || letra === "o" || letra === "u"){ 
-  retun "es vocal"}  retun "dato incorrecto"
+  return "Es vocal"}  return "Dato incorrecto"
 }
 
 
